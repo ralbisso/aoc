f4f7db5b209f6e6b3f18690491b91d487885468a
ralbisso <albisson.romain@gmail.com>
2020-12-16 18:42:32 +0100

    Day 13 - Part 1

--- src/main/java/aoc2020/Day13Part01.java
+++ src/main/java/aoc2020/Day13Part01.java
@@ -1,10 +1,42 @@
 package aoc2020;
 
-public class Day13Part01 {
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+import utils.AdventOfCode;
+import utils.FileConstants;
 
+public class Day13Part01 extends AdventOfCode {
+
+    public static int solve() {
+        List<String> notes = getData(FileConstants.AOC_2020_13);
+        Set<Integer> busesIDs = getBusIDs(notes.get(1));
+        int min = Integer.MAX_VALUE;
+        int earliest = 0;
+        int estimate = Integer.parseInt(notes.get(0));
+        for (int busID : busesIDs) {
+            int wait = busID - estimate % busID;
+            if (wait < min) {
+                min = wait;
+                earliest = wait * busID;
+            }
+        }
+        return earliest;
     }
 
+    private static Set<Integer> getBusIDs(String buses) {
+        String[] split = buses.split(",");
+        Set<Integer> ids = new TreeSet<>();
+        for (String s : split) {
+            if (!s.equals("x")) {
+                ids.add(Integer.parseInt(s));
+            }
+        }
+        return ids;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Answer: " + solve());
+    }
 }
