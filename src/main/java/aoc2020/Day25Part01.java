7da5b12666608fdfef72345ece8f1241aa92039c
ralbisso <albisson.romain@gmail.com>
2020-12-26 17:11:21 +0100

    Day 25

--- src/main/java/aoc2020/Day25Part01.java
+++ src/main/java/aoc2020/Day25Part01.java
@@ -2,9 +2,37 @@ package aoc2020;
 
 public class Day25Part01 {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+    public static long solve() {
+        int cardPublicKey = 9717666;
+        int doorPublicKey = 20089533;
+        int cardSecretLoop = getSecretLoop(7, cardPublicKey);
+        return getEncryptionKey(cardSecretLoop, doorPublicKey);
     }
 
+    private static int getSecretLoop(int subject, int publicKey) {
+        int value = 1;
+        int loop = 0;
+        while (value != publicKey) {
+            loop++;
+            value *= subject;
+            value %= 20201227;
+            if (value == publicKey) {
+                break;
+            }
+        }
+        return loop;
+    }
+
+    private static long getEncryptionKey(int secretLoop, int publicKey) {
+        long value = 1L;
+        for (int i = 0; i < secretLoop; i++) {
+            value *= publicKey;
+            value %= 20201227;
+        }
+        return value;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Answer: " + solve());
+    }
 }
