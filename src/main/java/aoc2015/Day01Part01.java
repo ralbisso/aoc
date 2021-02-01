c8869428a5a9b0ed2dfeb87d692fda90cec14bb7
ralbisso <albisson.romain@gmail.com>
2021-02-01 18:16:40 +0100

    day 1 simplify

--- src/main/java/aoc2015/Day01Part01.java
+++ src/main/java/aoc2015/Day01Part01.java
@@ -9,7 +9,7 @@ public class Day01Part01 extends AdventOfCode {
         String instructions = getLine(FileConstants.AOC_2015_01);
         int floor = 0;
         for (char c : instructions.toCharArray()) {
-            floor += (c == '(') ? 1 : (c == ')') ? -1 : 0;
+            floor += (c == '(') ? 1 : -1;
         }
         return floor;
     }
--- src/main/java/aoc2015/Day01Part02.java
+++ src/main/java/aoc2015/Day01Part02.java
@@ -11,7 +11,7 @@ public class Day01Part02 extends AdventOfCode {
         int position = 0;
         while (floor > -1) {
             char c = instructions.charAt(position);
-            floor += (c == '(') ? 1 : (c == ')') ? -1 : 0;
+            floor += (c == '(') ? 1 : -1;
             position++;
         }
         return position;
